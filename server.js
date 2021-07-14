bf3161b5832ebddc9bd1bf622f6fb85013669788
Cairo Flores <florescairo2902@gmail.com>
2021-07-13 22:13:50 -0700

    add MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 // DEPENDENCIES
 const express = require('express');
+const mongoose = require('mongoose');
 
 // CONFIGURATION
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+
+// DATABASE
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+// ERROR / SUCCESS
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+);
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI)
+);
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
 // ROUTES
 const choresController = require('./controllers/chores_controller');
