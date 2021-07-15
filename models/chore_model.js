c2f301bbb1385a1759af7aae7ea0ec8a4d6f52a7
Cairo Flores <florescairo2902@gmail.com>
2021-07-14 19:02:04 -0700

    write out seed data

--- models/chore_model.js
+++ models/chore_model.js
@@ -4,10 +4,14 @@ const choreSchema = new mongoose.Schema({
     choreName: String,
     img: { type: String, default: 'https://via.placeholder.com/150' },
     points: Number,
-    timeStamp: String,
+    date: String,
     personName: String
 });
 
 const Chore = mongoose.model('Chore', choreSchema);
 
 module.exports = Chore
+
+
+
+// BUILD A SEED
---
+++ models/seeds/Jun27Jul11.js
@@ -0,0 +1,500 @@
+module.exports = [
+// EMPTY DISHWASHER
+    // Cai
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Cai'
+    },
+    
+
+    // Kyle
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kyle'
+    },
+    
+
+    // Ana
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Ana'
+    },
+    
+
+    // Mathew
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Mathew'
+    },
+    
+
+    // Kat
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kat'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kat'
+    },
+    
+
+    // Nate
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    
+
+    // Alex
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    
+
+    // Phaedrus
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    
+// FILL DISHWASHER
+    // Cai
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Cai'
+    },
+    
+
+    // Ana
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Ana'
+    },
+    
+    
+    // Kyle
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kyle'
+    },
+    
+
+    // Nate
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    
+
+    // Alex
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+    
+
+    // Kat
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kat'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kat'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kat'
+    },
+    
+
+    // Phaedrus
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Fill Dishwasher",
+        img: 'https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    
+// EMPTY DRY RACK
+    // Kyle
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kyle'
+    },
+    
+
+    // Nate
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    
+
+    // Phaedrus
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    {
+        choreName: "Empty Dry Rack",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    
+// HAND WASH DISHES
+    // Kat
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kat'
+    },
+    
+
+    // Phaedrus
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Phaedrus'
+    },
+    
+
+    // Nate
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Nate'
+    },
+    
+
+    // Kyle
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kyle'
+    },
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Kyle'
+    },
+    
+
+    // Alex 
+    {
+        choreName: "Hand Wash Dishes",
+        img: 'https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg',
+        points: 5,
+        date: '07/11/2021',
+        personName: 'Alex'
+    },
+
+]
+
+// choreName: String,
+// img: { type: String, default: 'https://via.placeholder.com/150' },
+// points: Number,
+// date: String,
+// personName: String
+
+
+// images
+
+// Empty Dishwasher: 
+    // https://images.pexels.com/photos/3829549/pexels-photo-3829549.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+// Fill Dishwasher: 
+    // https://www.bora.com/fileadmin/_processed_/1/2/csm_2018_BORA_Pure_Leichte-Reinigung_184741f069.jpg
+// Empty Dry Rack
+    // https://i5.walmartimages.com/asr/e1998c94-a06b-43d7-9a56-7b968802417f.af61da76f90254e57f85f3f111642a14.jpeg
